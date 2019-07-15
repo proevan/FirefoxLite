3cb1d02110556854547cd233a3764be93badf1ae
proevan <yanghsin.lin@gmail.com>
2019-07-15 16:45:03 +0800

    [#3782] set up webview settings

--- app/src/main/java/org/mozilla/focus/FocusApplication.kt
+++ app/src/main/java/org/mozilla/focus/FocusApplication.kt
@@ -28,6 +28,7 @@ import org.mozilla.focus.search.SearchEngineManager
 import org.mozilla.focus.telemetry.TelemetryWrapper
 import org.mozilla.focus.utils.AdjustHelper
 import org.mozilla.focus.utils.Settings
+import org.mozilla.focus.web.WebViewProvider
 import org.mozilla.rocket.content.news.data.NewsSourceManager
 import org.mozilla.rocket.di.DaggerAppComponent
 import org.mozilla.rocket.partner.PartnerActivator
@@ -56,9 +57,22 @@ open class FocusApplication : LocaleAwareApplication(), HasSupportFragmentInject
         SystemEngine(this, engineSettings)
     }
     val engineSettings: DefaultSettings by lazy {
-        DefaultSettings().apply {
-            trackingProtectionPolicy = createTrackingProtectionPolicy(isInPrivateProcess)
-        }
+        DefaultSettings(
+            trackingProtectionPolicy = createTrackingProtectionPolicy(isInPrivateProcess),
+            displayZoomControls = false,
+            // To respect the html viewport:
+            loadWithOverviewMode = true,
+            // Disable access to arbitrary local files by webpages - assets can still be loaded
+            // via file:///android_asset/res, so at least error page images won't be blocked.
+            allowFileAccess = false,
+            allowFileAccessFromFileURLs = false,
+            allowUniversalAccessFromFileURLs = false,
+            userAgentString = WebViewProvider.getUserAgentString(this),
+            // Right now I do not know why we should allow loading content from a content provider
+            allowContentAccess = false,
+            // The default for those settings should be "false" - But we want to be explicit.
+            domStorageEnabled = true
+        )
     }
     val sessionManager: SessionManager by lazy {
         SessionManager(engine)
